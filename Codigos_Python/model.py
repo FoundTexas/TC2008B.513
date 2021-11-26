89a294a91b4d56353b923c62b92013979a9d9996
Victoria Vazquez <65380353+VickyA01654095@users.noreply.github.com>
2021-11-26 16:24:24 -0600

    Separación en 2 archivos

--- Codigos_Python/model.py
+++ Codigos_Python/model.py
@@ -6,11 +6,6 @@ from mesa.visualization.ModularVisualization import ModularServer
 from mesa.visualization.UserParam import UserSettableParameter
 import random
 
-from http.server import BaseHTTPRequestHandler, HTTPServer
-from flask import Flask, render_template, request, jsonify
-import logging
-import json, os, atexit
-
 class Semaforo(Agent):
     def __init__(self, unique_id, model,s):
       super().__init__(unique_id, model)
@@ -329,73 +324,3 @@ class CruceModel(Model):
             print(p)
             ps.append(p)
         return ps
-
-
-app = Flask(__name__, static_url_path = '')
-
-model = CruceModel(4,10,10)
-
-def positionsToJSON(ps):
-    posDICT = []
-    for p in ps:
-        pos = {
-            "x" : p[0],
-            "y" : p[1],
-            "z" : p[2]
-        }
-        posDICT.append(pos)
-    return json.dumps(posDICT)
-
-def BoolsToJSON(g):
-    posDICT = []
-    for p in g:
-        pos = {
-            "Green" : p.Green,
-            "Yellow" : p.Yellow,
-            "Red" : p.Red,
-            "Posx" : p.rpos[0],
-            "Posz" : p.rpos[1],
-            "dir" : p.dir
-        }
-        posDICT.append(pos)
-    return json.dumps(posDICT)
-
-def CallesToJSON(g):
-    posDICT = []
-    for p in g:
-        pos = {
-            "x" : p.pos[0],
-            "y" : -1,
-            "z" : p.pos[1]
-        }
-        posDICT.append(pos)
-    return json.dumps(posDICT)
-
-port = int(os.getenv('PORT',8585))
-
-@app.route('/')
-
-def root():
-    return jsonify([{"message":"Hello"}])
-
-@app.route('/calles', methods=['GET','POST'])
-
-def calles():
-    positions = model.Calles
-    return CallesToJSON(positions)
-
-@app.route('/muliagentes', methods=['GET','POST'])
-
-def multiagentes():
-    positions = model.step()
-    return positionsToJSON(positions)
-
-@app.route('/semaforos', methods=['GET','POST'])
-
-def semaforos():
-    lights = model.step2()
-    return BoolsToJSON(lights)
-    
-    
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',port = port, debug = True)
